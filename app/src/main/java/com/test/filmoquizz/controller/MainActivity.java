fea21329899c1991b66cef03bdd5ba79fadb0c24
0riginals <paul.vinot@supinfo.com>
2020-06-02 18:41:52 +0200

    Initial commit

---
+++ app/src/main/java/com/test/filmoquizz/controller/MainActivity.java
@@ -0,0 +1,16 @@
+package com.test.filmoquizz.controller;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.test.filmoquizz.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
