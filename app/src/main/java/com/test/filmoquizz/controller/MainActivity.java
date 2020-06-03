1a24c3a5f144ad1eeaa7432fc4089385e0adf61c
0riginals <paul.vinot@supinfo.com>
2020-06-03 22:25:38 +0200

    Mise à jour du layout activity_main.xml et du controller MainActivity

--- app/src/main/java/com/test/filmoquizz/controller/MainActivity.java
+++ app/src/main/java/com/test/filmoquizz/controller/MainActivity.java
@@ -3,15 +3,39 @@ package com.test.filmoquizz.controller;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.test.filmoquizz.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    // On bind les éléments que nous allons avoir besoin.
+    private EditText pseudoLogin;
+    private EditText pseudoPassword;
+    private Button loginButton;
+    private EditText pseudoRegister;
+    private EditText passwordRegister;
+    private EditText confirmPasswordRegister;
+    private Button registerButton;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        // Ceci est un commentaire très utile kek
+
+        // On vient connecter les différents éléments du layout sur notre méthode onCreate
+        // Les éléments pour la connexion
+        pseudoLogin = (EditText) findViewById(R.id.activity_main_login_username);
+        pseudoPassword = (EditText) findViewById(R.id.activity_main_login_password);
+        loginButton = (Button) findViewById(R.id.activity_main_login_submit);
+
+        // Les éléments pour l'enregistrement
+        pseudoRegister = (EditText) findViewById(R.id.activity_main_register_pseudo);
+        passwordRegister = (EditText) findViewById(R.id.activity_main_register_password);
+        confirmPasswordRegister = (EditText) findViewById(R.id.activity_main_register_pseudo_confirm);
+        registerButton = (Button) findViewById(R.id.activity_main_register_submit);
+
     }
 }
--- app/src/main/java/com/test/filmoquizz/controller/QuizzActivity.java
+++ app/src/main/java/com/test/filmoquizz/controller/QuizzActivity.java
@@ -1,8 +1,10 @@
-package com.test.filmoquizz;
+package com.test.filmoquizz.controller;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.test.filmoquizz.R;
 
 public class QuizzActivity extends AppCompatActivity {
 
