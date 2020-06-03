43beed57bccf5674d8e50dd5eaccf9498e20757b
Antoine <59antoine620@gmail.com>
2020-06-03 20:53:47 +0200

    Ajout QuizzActivity

--- app/src/main/java/com/test/filmoquizz/HomeActivity.java
+++ app/src/main/java/com/test/filmoquizz/HomeActivity.java
@@ -2,13 +2,35 @@ package com.test.filmoquizz;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 public class HomeActivity extends AppCompatActivity {
+
+    // Elements de HomeActivity
+    private ImageButton playQuizzButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        playQuizzButton = (ImageButton) findViewById(R.id.playQuizz);
+
+
+        playQuizzButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            // méthode permettant de changer d'activité après le clique sur l'image
+            public void onClick(View v) {
+                Intent quizzActivityIntent = new Intent(HomeActivity.this, QuizzActivity.class);
+                startActivity(quizzActivityIntent);
+            }
+        });
     }
+
+
 }
